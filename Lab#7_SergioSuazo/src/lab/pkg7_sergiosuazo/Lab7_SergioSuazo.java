007437486f939c0788309e0cd2d0c023d889a155
Sergio Suazo <sergiosb1001@gmail.com>
2019-11-29 13:24:36 +0800

    inicio

---
+++ Lab#7_SergioSuazo/src/lab/pkg7_sergiosuazo/Lab7_SergioSuazo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg7_sergiosuazo;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Lab7_SergioSuazo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
